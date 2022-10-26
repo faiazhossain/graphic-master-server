407f624c625d475d01aa4492681553143c784e87
Faiaz Hossain <faiazhossain10@gmail.com>
2022-10-26 08:21:18 +0600

    added courseInfo_id

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 
+app.use(cors());
 const courses = require("./data/courses.json");
-
+const courseInfo = require("./data/course-info.json");
 app.get("/", (req, res) => {
   res.send("Course API Running");
 });
 
 app.get("/courses", (req, res) => {
   res.send(courses);
+});
+
+app.get("/courseInfo/:id", (req, res) => {
+  console.log(req.params);
 });
 
 app.listen(port, () => {
