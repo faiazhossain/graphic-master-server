e93fcbc37a39aa021e6b064c06750b22f188088b
Faiaz Hossain <faiazhossain10@gmail.com>
2022-10-27 15:39:52 +0600

    added courses route

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ const courseInfo = require("./data/course-info.json");
 app.get("/", (req, res) => {
   res.send("Course API Running");
 });
+app.get("/courses", (req, res) => {
+  res.send(courses);
+});
+app.get("/courses/:id", (req, res) => {
+  const id = req.params.id;
+  const category_course = courseInfo.filter((c) => c.course_id === id);
+  res.send(category_course);
+});
 
 app.listen(port, () => {
   console.log("Graphic Master Server running on port", port);
